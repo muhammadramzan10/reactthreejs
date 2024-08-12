8a15646a6f8f1707096023dc875955f7e103c17c
muhammadramzan10 <Zacklancer13@gmail.com>
2024-08-12 22:41:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import Customizer from './pages/Customizer'
+import Canvas from './canvas'
+
+
+function App() {
+
+
+  return (
+    <main className='app transition-all ease-in'>
+      <Home />  
+      <Customizer />
+      <Canvas />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import ColorPicker from "./ColorPicker";
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+
+export { ColorPicker, CustomButton, FilePicker, Tab}
